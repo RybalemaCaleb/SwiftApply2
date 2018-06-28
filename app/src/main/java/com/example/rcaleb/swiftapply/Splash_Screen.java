86494da00994201a41a4325d9fb1efe686d5c6ff
rcaleb <caleb.rubalema@gmail.com>
2018-06-28 15:01:27 +0300

    bringing all

--- app/src/main/java/com/example/rcaleb/swiftapply/Splash_Screen.java
+++ app/src/main/java/com/example/rcaleb/swiftapply/Splash_Screen.java
@@ -1,5 +1,6 @@
 package com.example.rcaleb.swiftapply;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,24 @@ public class Splash_Screen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+        Thread waiter = new Thread(){
+            @Override
+            public void run() {
+
+                try {
+                    sleep(4000);
+                } catch (InterruptedException e) {
+
+                    e.printStackTrace();
+                }finally {
+                    Intent next = new Intent(getApplicationContext(),application_status.class);
+                    startActivity(next);
+                    finish();
+                }
+            }
+        };
+
+        waiter.start();
+
     }
 }
---
+++ app/src/main/java/com/example/rcaleb/swiftapply/application_status.java
@@ -0,0 +1,13 @@
+package com.example.rcaleb.swiftapply;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class application_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_application_status);
+    }
+}
