0aaa63c7903d9039d0355263011e1bf0823a66fd
rcaleb <caleb.rubalema@gmail.com>
2018-06-30 16:55:36 +0300

    bringing all

--- app/src/main/java/com/example/rcaleb/swiftapply/StatusDataHolder.java
+++ app/src/main/java/com/example/rcaleb/swiftapply/StatusDataHolder.java
@@ -1,9 +1,19 @@
 package com.example.rcaleb.swiftapply;
 
+import org.json.JSONArray;
+
 public class StatusDataHolder {
     private String name;
     private int img;
-    private int status;
+    private String sex;
+    private String Date;
+    private String Class_to_go_to;
+    private String Term_to_go_to;
+    private String parent_name;
+    private String parent_no;
+    private String district_name;
+    private String village_name;
+    private JSONArray marks;
 
     public String getName() {
         return name;
@@ -21,11 +31,75 @@ public class StatusDataHolder {
         this.img = img;
     }
 
-    public int getStatus() {
-        return status;
+    public String getSex() {
+        return sex;
     }
 
-    public void setStatus(int status) {
-        this.status = status;
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getDate() {
+        return Date;
+    }
+
+    public void setDate(String date) {
+        Date = date;
+    }
+
+    public String getClass_to_go_to() {
+        return Class_to_go_to;
+    }
+
+    public void setClass_to_go_to(String class_to_go_to) {
+        Class_to_go_to = class_to_go_to;
+    }
+
+    public String getTerm_to_go_to() {
+        return Term_to_go_to;
+    }
+
+    public void setTerm_to_go_to(String term_to_go_to) {
+        Term_to_go_to = term_to_go_to;
+    }
+
+    public String getParent_name() {
+        return parent_name;
+    }
+
+    public void setParent_name(String parent_name) {
+        this.parent_name = parent_name;
+    }
+
+    public String getParent_no() {
+        return parent_no;
+    }
+
+    public void setParent_no(String parent_no) {
+        this.parent_no = parent_no;
+    }
+
+    public String getDistrict_name() {
+        return district_name;
+    }
+
+    public void setDistrict_name(String district_name) {
+        this.district_name = district_name;
+    }
+
+    public String getVillage_name() {
+        return village_name;
+    }
+
+    public void setVillage_name(String village_name) {
+        this.village_name = village_name;
+    }
+
+    public JSONArray getMarks() {
+        return marks;
+    }
+
+    public void setMarks(JSONArray marks) {
+        this.marks = marks;
     }
 }
